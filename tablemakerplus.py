7313066b33a66aa630035084c2979517ac03297a
reeselikethepieces <charisse.carlson@gmail.com>
2025-10-30 17:34:40 -0700

    new

---
+++ tablemakerplus.py
@@ -0,0 +1,23 @@
+# write a program that:
+'''
+ - takes parameters (csv_filename, link) 
+ 	
+ 	- create table 
+ 		1 (A-K - intentionally global, rows)
+ 			r1 = catnames across columns (A-K)
+ 		2 (A-C, 5)
+ 	
+ 	- rules for table
+ 		
+ 	
+ 	- for csv_filname
+ 		isolates two columns
+ 			put into table as columns EF
+ 		take (column A) of line7
+ 			hold list as a variable
+ 	
+ 	- for link
+ 		use as link to connect to columnA, row2
+ 		then, change ##### at the end of the link to match next in row
+ 		
+'''
