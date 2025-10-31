433236f461682ae366c672295c77dbeb4e18b15d
reeselikethepieces <charisse.carlson@gmail.com>
2025-10-31 13:22:14 -0700

    20251031_korflab

--- hom_het_countsNcompare.py
+++ hom_het_countsNcompare.py
@@ -9,20 +9,21 @@ rounds = 10
 coverage = 24
 	# 20 doesnt show much, so started with 24
 	# change to 30
+	# try 40
 
 # HET
 het = [0] * (coverage+1) 
 
 for i in range(rounds):
 	for j in range(coverage):
-		mom = 0
+		mom = 0 # ref allele
 		dad = 0
 			if random.randint(0, 1) == 0: mom += 1
 			else:                         dad += 1
 		het[mom] += 1
 		
 for i, c in enumerate(het):
-	print('het', i, coverage-i, c, sep='/t')
+	print('het', i, coverage-i, c, sep='\t')
 
 # HOM
 mut = 0.01
@@ -32,10 +33,10 @@ for _ in range(trials):
 	g = 0
 	t = 0
 	for _ in range(coverage):
-		r = random.randint(0, 2)
-		if   r == 0: c += 1
-		elif r == 1: g += 1
-		else:        t += 1
+		if r = random.randint(0, 2)
+			if   r == 0: c += 1
+			elif r == 1: g += 1
+			else:        t += 1
 	a = [c, g, t]
 	# if a or b have value in them
 	#if a[0] or a[1]: continue
